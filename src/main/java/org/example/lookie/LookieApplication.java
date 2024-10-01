8b7f49caacc7c49aa412808224ae2be26bc7b1d8
JiinHong <com5942@seoultech.ac.kr>
2024-10-01 19:09:02 +0900

    git init

---
+++ src/main/java/org/example/lookie/LookieApplication.java
@@ -0,0 +1,13 @@
+package org.example.lookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookieApplication.class, args);
+    }
+
+}
