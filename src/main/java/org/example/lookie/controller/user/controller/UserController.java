0fe2c1ed28c5ada108193ea8b810da3b67e070d1
JiinHong <com5942@seoultech.ac.kr>
2024-10-30 19:06:19 +0900

    feat: 카카오 로그인 구현 & S3 Presigned URL 구현 & Swagger 세팅

--- src/main/java/org/example/lookie/controller/user/controller/UserController.java
+++ src/main/java/org/example/lookie/controller/user/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/api/user")
+@RequestMapping("/api/v1/user")
 public class UserController implements UserControllerDocs {
 
     private final UserService userService;
