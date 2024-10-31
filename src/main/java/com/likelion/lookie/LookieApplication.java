489984f175f97039ad6d82f24220ce9e48b321b8
thdus <sykim0659@naver.com>
2024-10-31 21:52:43 +0900

    fix:deploy

---
+++ src/main/java/com/likelion/lookie/LookieApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.lookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookieApplication.class, args);
+	}
+
+}
