e76db43a0ab8dfb2e86ab9e89ab073e9329ee6f5
ParkJinHong <com5942@seoultech.ac.kr>
2024-11-04 02:31:48 +0900

    Update LookieApplicationTests.java

--- src/test/java/com/likelion/lookie/LookieApplicationTests.java
+++ src/test/java/com/likelion/lookie/LookieApplicationTests.java
@@ -1,15 +1,13 @@
 package com.likelion.lookie;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled // 테스트를 임시로 무시
 class LookieApplicationTests {
 
     @Test
     void contextLoads() {
-        // 테스트 내용
+        
     }
 }
