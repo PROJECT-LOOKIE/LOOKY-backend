e8295d660bc7db71ab09f60b76c7001753624854
JiinHong <com5942@seoultech.ac.kr>
2024-11-15 17:34:03 +0900

    feat: repository 생성

---
+++ src/main/java/com/likelion/lookie/repository/ClothesLookRepository.java
@@ -0,0 +1,7 @@
+package com.likelion.lookie.repository;
+
+import com.likelion.lookie.entity.ClothesLook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClothesLookRepository extends JpaRepository<ClothesLook, Long> {
+}
