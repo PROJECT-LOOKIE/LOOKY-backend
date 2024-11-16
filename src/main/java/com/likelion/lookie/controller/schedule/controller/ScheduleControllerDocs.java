62abc4494e544acaf1895b18d5eed1be2adcc3d6
JiinHong <com5942@seoultech.ac.kr>
2024-11-16 17:11:37 +0900

    feat: 메인-약속 초대 API (swagger 추가)

--- src/main/java/com/likelion/lookie/controller/schedule/controller/ScheduleControllerDocs.java
+++ src/main/java/com/likelion/lookie/controller/schedule/controller/ScheduleControllerDocs.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Schedule", description = "Schedule API")
@@ -30,4 +31,15 @@ public interface ScheduleControllerDocs {
             @RequestBody CreateScheduleRequestDto registerScheduleRequestDto);
 
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
+                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR",
+                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})
+    })
+    @Operation(summary = "메인-약속 초대", description = "약속 초대 API")
+    ApplicationResponse<String> inviteSchedule(
+            @AuthenticationPrincipal UserInfoDTO userInfoDTO,
+            @PathVariable("schedule_id") Long scheduleId);
 }
