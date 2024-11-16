dadb80a0b5145e50adf8c39f77020fd7e0ab53f8
JiinHong <com5942@seoultech.ac.kr>
2024-11-16 12:25:36 +0900

    feat: Repository 생성

---
+++ src/main/java/com/likelion/lookie/repository/LookRepository.java
@@ -0,0 +1,7 @@
+package com.likelion.lookie.repository;
+
+import com.likelion.lookie.entity.Look;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LookRepository extends JpaRepository<Look, Long> {
+}
