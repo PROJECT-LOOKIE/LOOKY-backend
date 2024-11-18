7b2e340e2d54bf4a59ebb2ccce621afa296f4895
JiinHong <com5942@seoultech.ac.kr>
2024-11-18 22:54:30 +0900

    feat: 사용자 정보 수정 로직 수정

--- src/main/java/com/likelion/lookie/repository/UserRepository.java
+++ src/main/java/com/likelion/lookie/repository/UserRepository.java
@@ -2,13 +2,17 @@ package com.likelion.lookie.repository;
 
 import com.likelion.lookie.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByNickname(String nickname); // 닉네임 중복 체크
+    // 현재 사용자를 제외한 닉네임 중복 체크
+    @Query("SELECT COUNT(u) > 0 FROM User u WHERE u.nickname = :nickname AND u.id != :userId")
+    boolean existsByNicknameAndNotUserId(@Param("nickname") String nickname, @Param("userId") Long userId);
 
     Optional<User> findBySubId(String subId);
 
